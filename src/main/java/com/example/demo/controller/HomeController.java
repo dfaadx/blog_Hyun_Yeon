29a7673c7fe4da2bb002dc7c461f45bb7f64d822
yeonji <yeonji@192.168.124.102>
2021-10-04 10:05:24 +0900

    home init

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,18 +1,30 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.domain.BoardDTO;
+import com.example.demo.service.BoardService;
 
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private BoardService boardService;
 
-	@GetMapping(value = "/home")
+	@GetMapping(value = {"/home","/"})
 	public String home(Model model) {
 
-		System.out.println(this.getClass().getName() + " [/home]");
+		System.out.println(this.getClass().getName() + " [/, /home]");
+		
+		List<BoardDTO> boardList = boardService.getBoardList();
+		model.addAttribute("boardList", boardList);
+		
 		return "blog/home";
 		
 	}
